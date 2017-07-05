6f2ea2972dc76a0e3ed271aca591e9c9bba0ce40
Antti Aalto <antti.aalto.10@gmail.com>
2017-07-05 20:51:33 +0300

    initial commit

---
+++ hero.py
@@ -0,0 +1,114 @@
+import xml.etree.ElementTree as xmltree
+import copy
+
+import data
+import model
+import os
+
+class Alt:
+	def __init__(self, xml):
+		pass
+
+
+class Hero:
+	def __init__(self, xml, filename, abilities):
+		self.xml = xml
+		self.filename = filename
+		self.abilities = []
+		for i in range(0,4):
+			if "inventory" + str(i) in abilities:
+				self.abilities.push_back(abilities[xml.get("inventory" + str(i))])
+		self.modelpath = self.absolutepath(xml.get("model"))
+		self.mainModel = model.Model(data.models[self.modelpath])
+
+	def getAlts(self):
+		return [alt.get("key").split(".")[1] if len(alt.get("key").split(".")) > 1 else alt.get("key") for alt in self.xml.findall("altavatar")]
+
+	def absolutepath(self, path):
+		if path[0] == "/":
+			return path
+		return os.path.dirname(self.filename) + "/" + path
+		
+	def generate(self, alt=""):
+		editedfiles = dict()
+
+		if self.xml is None:
+			print(self.filename)
+			return dict()
+
+		editedxml = copy.deepcopy(self.xml)
+
+		changes = xmltree.Element("dummy")
+
+		projectile = None
+		projectilesToEdit = []
+
+		if "attackprojectile" in self.xml.attrib:
+			projectile = self.xml.get("attackprojectile")
+		for altTag in self.xml.findall("altavatar"):
+			if alt != "" and alt in altTag.get("key"):
+				changes = altTag
+				if "attackprojectile" in altTag.attrib:
+					projectile = altTag.get("attackprojectile")
+				break
+		for key,value in changes.attrib.items():
+			editedxml.set(key, value)
+		for child in list(changes):
+			toremove = editedxml.find(child.tag)
+			if toremove is not None:
+				editedxml.remove(toremove)
+		editedxml.extend(list(changes))
+
+		newmodelpath = self.absolutepath(editedxml.get("model"))
+		newModel = model.Model(data.models[newmodelpath])
+
+		for altTag in editedxml.findall("altavatar"):
+			name = altTag.get("key")
+			attackprojectile = altTag.get("attackprojectile")
+			altModelPath = altTag.get("model")
+			altTag.clear()
+			altTag.tail = "\n"
+			altTag.set("key", name)
+			altTag.set("altavatar", "true")
+			altTag.set("modpriority", "1")
+			if attackprojectile is not None:
+				altTag.set("attackprojectile", attackprojectile)
+				projectilesToEdit.append(attackprojectile)
+			if altModelPath is not None:
+				altTag.set("model", altModelPath)
+				altModelPath = self.absolutepath(altModelPath)
+				if altModelPath in data.models:
+					attribs = dict()
+					for attribute in ["file", "high", "low", "med"]:
+						if attribute in data.models[newmodelpath].attrib:
+							attribs[attribute] = "/" + self.absolutepath(data.models[newmodelpath].get(attribute))
+					altModel = model.Model(data.models[altModelPath])
+					editedfiles[altModelPath] = xmltree.tostring(newModel.generate(altModel.animorder, attribs, "/" + os.path.dirname(newmodelpath) + "/"), encoding="unicode")
+		editedfiles[self.filename] = xmltree.tostring(editedxml, encoding="unicode")
+
+
+
+		for ability in self.abilities:
+			editedAbility = copy.deepcopy(ability[0])
+			changes = xmltree.Element("dummy")
+			for altTag in ability[0].findall("altavatar"):
+				if alt != "" and alt in altTag.get("key"):
+					changes = altTag
+					break
+			for key,value in changes.attrib.items():
+				editedAbility.set(key, value)
+			for child in list(changes):
+				toremove = editedxml.find(child.tag)
+				if toremove is not None:
+					editedAbility.remove(toremove)
+			for altTag in editedAbility.findall("altavatar"):
+				name = altTag.get("key")
+				projectile = altTag.get("projectile")
+				altTag.clear()
+				altTag.tail = "\n"
+				altTag.set("name", name)
+				if projectile is not None:
+					altTag.set("projectile", projectile)
+			editedfiles[ability[1]] = xmltree.tostring(editedAbility, encoding="unicode")
+
+		return editedfiles
---
+++ main.py
@@ -0,0 +1,59 @@
+honpath = "C:\\Program Files (x86)\\Heroes of Newerth"
+
+import zipfile
+import os
+import xml.etree.ElementTree as xmltree
+
+import hero
+import data
+
+zipfiles = dict()
+def getzip(path):
+	if path not in zipfiles.keys():
+		zipfiles[path] = zipfile.ZipFile(path)
+	return zipfiles[path]
+
+files = dict()
+
+resources = [os.path.join(honpath, "game", f) for f in os.listdir(os.path.join(honpath, "game")) if (os.path.isfile(os.path.join(honpath, "game", f)) and f[0:len("resources")] == "resources" and f[-len(".s2z"):] == ".s2z" and f != "resources999.s2z" )]
+
+resources.sort()
+
+translationsfile = getzip(os.path.join(honpath, "game/resources0.s2z")).open("stringtables/entities_en.str")
+for line in translationsfile.readlines():
+	line = line.decode("utf-8").strip()
+	if line == "" or line[0:1] == "//":
+		continue
+	split=line.split()
+	data.translations[split[0]] = " ".join(split[1:])
+
+for resource in resources:
+	zip = getzip(resource)
+	for file in zip.namelist():
+		files[file] = resource
+		if file[-len(".entity"):] == ".entity":
+			xml = xmltree.fromstring(zip.open(file).read().decode("utf-8"))
+			collection = None
+			if xml.tag == "hero":
+				collection = data.heroes
+			if xml.tag == "ability":
+				collection = data.abilities
+			if xml.tag == "item":
+				collection = data.items
+			if xml.tag == "projectile":
+				collection = data.projectiles
+			if collection is not None:
+				collection[xml.attrib["name"]] = [xml, file]
+		if file[-len(".mdf"):] == ".mdf":
+			data.models[file] = xmltree.fromstring(zip.open(file).read().decode("utf-8"))
+
+modzip = zipfile.ZipFile(os.path.join(honpath, "game/resourcesAutomaticAlts.s2z"), "w")
+for entityname, (xml, file) in data.heroes.items():
+	heroobject = hero.Hero(xml, file, data.abilities)
+	for filename, content in heroobject.generate().items():
+		if content is not None:
+			modzip.writestr(filename, content)
+modzip.close()
+
+for name in zipfiles:
+	zipfiles[name].close()
